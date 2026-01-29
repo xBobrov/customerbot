deb928ae602a4f3e58160f616d981007102301dd
xBobrov <andy.beaver.vld@gmail.com>
2026-01-29 16:00:02 +1000

    + commit

---
+++ src/main/java/com/vodokanal/customerbot/CustomerbotApplication.java
@@ -0,0 +1,13 @@
+package com.vodokanal.customerbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerbotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vodokanal/customerbot/CustomerbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vodokanal.customerbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
